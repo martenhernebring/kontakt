6c168b3190bf69f0466e9c7748d42329faef235c
martenhernebring <72164095+martenhernebring@users.noreply.github.com>
2021-01-29 16:04:17 +0100

    Removed old code from TestMain.java

--- src/test/java/se/mns/TestMain.java
+++ src/test/java/se/mns/TestMain.java
@@ -2,20 +2,6 @@ package se.mns;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import org.junit.jupiter.api.Test;
-
 class TestMain {
-
-    @Test
-    public void printHelloWorld() {
-        final PrintStream standardOut = System.out;
-        final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStreamCaptor));
-        Main.main(null);
-        assertEquals("Hello World!" + System.lineSeparator(), outputStreamCaptor.toString());
-        System.setOut(standardOut);
-    }
 
 }
