816d0d17cd30ab994fbb23500688e7c8390d935c
Your Name <marten.hernebring@gmail.com>
2021-02-01 17:26:44 +0100

    Customer will be showned red or green depending on IllegalArgumentException thrown

--- src/main/java/se/mns/CustomerInteraction.java
+++ src/main/java/se/mns/CustomerInteraction.java
@@ -7,17 +7,25 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+
+/*Will be used for animations
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;*/
 
 import javax.swing.JPanel;
-import javax.swing.Timer;
 
-/**
- * @author Mårten Hernebring
- * @version 1
- *
- */
 public class CustomerInteraction extends JPanel {
+    private static final long serialVersionUID = -783845033008947920L;
+    
+    private boolean error = true;
+    private String message = "Error!";
+
+    public void setMessage(String message) {
+        this.error = false;
+        this.message = message;
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -25,19 +33,21 @@ public class CustomerInteraction extends JPanel {
         repaint(g);
     }
 
-    /**
-     * Call this method when the graphics needs to be repainted on the graphics
-     * surface. Simple version just displays error message
-     * 
-     * @param g the graphics to paint on
-     */
     private void repaint(Graphics g) {
         final Dimension d = this.getSize();
-        g.setColor(Color.red);
-        g.fillRect(0, 0, d.width, d.height);
-        g.setColor(Color.black);
-        g.setFont(new Font("Arial", Font.BOLD, 48));
-        g.drawString("Error!", 20, d.width / 2 - 24);
+        if(error) {
+            g.setColor(Color.red);
+            g.fillRect(0, 0, d.width, d.height);
+            g.setColor(Color.black);
+            g.setFont(new Font("Arial", Font.BOLD, 48));
+            g.drawString(message, 20, d.width / 2 - 24);
+        } else {
+            g.setColor(Color.green);
+            g.fillRect(0, 0, d.width, d.height);
+            g.setColor(Color.black);
+            g.setFont(new Font("Arial", Font.BOLD, 48));
+            g.drawString(message, 20, d.width / 2 - 24);
+        }
     }
 
 }
--- src/main/java/se/mns/Main.java
+++ src/main/java/se/mns/Main.java
@@ -5,25 +5,33 @@ package se.mns;
 
 /**
  * @author Mårten Hernebring
- * @version 1
+ * @version 2
  *
  */
 
 import javax.swing.JFrame;
 
 public class Main {
-
-    /**
-     * @param args not used
-     */
+    
     public static void main(String[] args) {
-        JFrame main = new JFrame("Error message");
+        //Eclipse run conf: 123 mail@test.com 031-288339
+        ContactBook contacts = new ContactBook();
+        var customerInteraction = new CustomerInteraction();
+        try {
+            Contact contact = new Contact(args[0], args[1], args[2]);
+            contacts.add(contact);
+            customerInteraction.setMessage("Lagt till kontakten.");
+        } catch (IllegalArgumentException ex) {
+            //error will show on default
+        }
+        showMessage(customerInteraction);
+    }
 
-        CustomerInteraction ci = new CustomerInteraction();
-
-        main.setSize(400, 400);
+    private static void showMessage(CustomerInteraction customerInteraction) {
+        JFrame main = new JFrame("Kontaktbok");
+        main.setSize(800, 800);
         main.setResizable(false);
-        main.add(ci);
+        main.add(customerInteraction);
         main.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
         main.setVisible(true);
     }
