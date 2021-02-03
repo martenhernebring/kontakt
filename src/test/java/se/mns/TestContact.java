0c9535f37c6ac4b4576f5efe873e570af6780fc5
suzyrgo <72145938+suzyrgo@users.noreply.github.com>
2021-02-03 19:33:35 +0100

    Update TestContact.java
    Add  test name ,phonenumber and email in testcontact

--- src/test/java/se/mns/TestContact.java
+++ src/test/java/se/mns/TestContact.java
@@ -8,22 +8,45 @@ public class TestContact {
 
 	Contact contact= new Contact("Naoya", "naoya@testemail.com", "07342567");
 	
-	@Test
-	public void testString() {
-		
-		assertTrue(contact.getName() instanceof String);
-	}
 	
+
 	@Test
 	public void testEqualsName() {
 		assertEquals("Naoya", contact.getName());
-		
+
 	}
-	
+
 	@Test
-	public void testEqualsNameWithSpaces() {
-		contact.setName(" Naoya " );
-		assertEquals("Naoya", contact.getName());
-		
+	public void testSetName() {
+		contact.setName(" Suzana ");
+		assertEquals("Suzana", contact.getName());
 	}
+
+
+	@Test
+	public void testGetEmail() {
+		assertEquals("naoya@testemail.com", contact.getEmail());
+	}
+
+	@Test
+	public void testGetPhoneNumber() {
+		assertEquals("07342567", contact.getPhoneNumber());
+	}
+
+	@Test
+	public void testSetEmail() {
+		contact.setEmail("suzana@hotmail.se");
+		assertEquals("suzana@hotmail.se", contact.getEmail());
+
+	}
+
+	@Test
+	public void testSetPhoneNumber() {
+		contact.setPhoneNumber("09356487");
+		assertEquals("09356487", contact.getPhoneNumber());
+
+
+	}	
+
+	
 }
